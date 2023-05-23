33416777c21e02862fef314c8866e0327afe1c66
Алмаз <zzdl4@bk.ru>
2023-05-23 22:15:00 +0300

    Done

--- 3_dict_to_csv.py
+++ 3_dict_to_csv.py
@@ -9,13 +9,24 @@
 2. Запишите содержимое списка словарей в файл в формате csv
 
 """
+import csv
+
 
 def main():
-    """
-    Эта функция вызывается автоматически при запуске скрипта в консоли
-    В ней надо заменить pass на ваш код
-    """
-    pass
+    data_list = [
+        {'name': 'John', 'age': 26, 'job': 'economist'},
+        {'name': 'Debra', 'age': 35, 'job': 'accountant'},
+        {'name': 'Ben', 'age': 14, 'job': 'unemployed'},
+        {'name': 'Bob', 'age': 42, 'job': 'programmer'},
+        {'name': 'Silvia', 'age': 21, 'job': 'designer'}
+    ]
+
+    with open('data.csv', 'w', newline='') as file:
+        titles = ['name', 'age', 'job']
+        writer = csv.DictWriter(file, titles, delimiter=';')
+        writer.writeheader()
+        for user in data_list:
+            writer.writerow(user)
 
 if __name__ == "__main__":
     main()

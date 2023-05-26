eb4220d82eb895cf0a38c3de39631a23f6af0395
БадриевА <zzdl4@bk.ru>
2023-05-26 15:11:17 +0300

    done

--- 2_files.py
+++ 2_files.py
@@ -12,14 +12,26 @@
 """
 
 
+def count_string_len(text):
+    return len(text)
+
+
+def count_words_in_text(text):
+    return len(text.split())
+
+
+def replace_dots_with_exclamation_marks(text):
+    return text.replace('.', '!')
+
+
 def main():
     with open('referat.txt', encoding='UTF-8') as read_file:
         read_txt = read_file.read()
 
-    print(f'Длина строки: {len(read_txt)}')
-    print(f'Количество слов в тексте: {len(read_txt.split())}')
-    read_txt = read_txt.replace('.', '!')
-    
+    print(f'Длина строки: {count_string_len(read_txt)}')
+    print(f'Количество слов в тексте: {count_words_in_text(read_txt)}')
+    read_txt = replace_dots_with_exclamation_marks(read_txt)
+
     with open('referat2.txt', 'w', encoding='UTF-8') as write_file:
         write_file.write(read_txt)
 
